702e878f14b39d4450ba165c1e9bdb60aa77a64f
Moritz Kirstein <mkirstein@protonmail.com>
2024-02-27 13:12:55 +0100

    feat: add start scripts

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,29 @@ import { createPublishAuditTrail } from './audit'
 import { createConsumptionReport } from './consumption'
 dotenv.config()
 
-// createPublishAuditTrail()
+async function main() {
+  const args = process.argv
+  const index = args.findIndex((arg) => arg.match(/[-]?-t[ype]?/))
+  if (index < 0) {
+    console.error(
+      'Could not find required argument for type. Use -t or --type to set a type.'
+    )
+    return
+  }
 
-createConsumptionReport()
+  const type = args[index + 1]
+  switch (type) {
+    case 'audit':
+      await createPublishAuditTrail()
+      break
+    case 'consumption':
+      await createConsumptionReport()
+      break
+    default:
+      console.error(
+        "Please choose a type of report to run. Possible options: 'audit', 'consumption'"
+      )
+  }
+}
+
+main()
